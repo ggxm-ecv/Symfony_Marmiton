7e964b86b885f1891610b784c0fc42efe098f642
Gautier <hello.gautiermaire@gmail.com>
2021-11-03 16:21:20 +0100

    login/register

---
+++ src/Controller/MainController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller;
+
+// use App\Entity\Blog;
+// use App\Services\Blogs;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+
+class MainController extends AbstractController
+{
+ 
+}
