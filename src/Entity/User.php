f626489f011d9597b688a67a28b71546029fd944
Vincent <31652038+Vincent-Manach@users.noreply.github.com>
2021-11-04 14:58:55 +0100

    Fix bug

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -199,8 +199,5 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
         }
 
         return $this;
-    public function __toString()
-    {
-        return $this->email;
     }
 }
